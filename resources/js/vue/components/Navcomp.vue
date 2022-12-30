54115a9fa241a1175d68017bcde3a766b5d3aef9
ben Khaireh <62518373+benkhaireh@users.noreply.github.com>
2022-12-30 18:09:43 +0300

    .

--- resources/js/vue/components/Navcomp.vue
+++ resources/js/vue/components/Navcomp.vue
@@ -14,12 +14,7 @@
             </a>
             <div class="flex items-center" v-click-away="hidenav">
                 <div class="flex md:order-2">
-                    <button
-                        type="button"
-                        class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-3 md:mr-0"
-                    >
-                    Parlez à un conseiller
-                    </button>
+                    <a href="#" class="px-[1.5rem] py-[0.5rem] border-2 border-[#39b54a] bg-[#39b54a] rounded-[3.5625rem] text-white text-xs font-[600] hover:bg-green-700 focus:bg-green-700 hover:border-green-700 focus:border-green-700">Talk to a Consultant</a>
                     <button
                         @click="sticknav"
                         type="button"
--- resources/js/vue/views/Home.vue
+++ resources/js/vue/views/Home.vue
@@ -8,6 +8,7 @@
                             Farming Software and Tools for the <span class="text-[#39b54a]">Modern Farmer</span>
                         </h1>
                         <p class="font-[500] mb-[2rem]">Synnefa offers smart and comprehensive farming tools with a variety of products to help the modern farmer take the guesswork out of farming.</p>
+                        <a href="#" class="px-[1.5rem] py-[0.75rem] border-2 border-[#39b54a] bg-[#39b54a] rounded-[3.5625rem] text-white text-[0.875rem] font-[600] hover:bg-green-700 focus:bg-green-700 hover:border-green-700 focus:border-green-700">Talk to a Consultant</a>
                     </div>
                 </div>
                 <div class="flex items-center justify-center">
