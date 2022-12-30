8f996a5f88619d0cc1b7d842df9c4849629ffb9b
ben Khaireh <62518373+benkhaireh@users.noreply.github.com>
2022-12-30 17:55:48 +0300

    Home changes

--- resources/js/vue/views/Home.vue
+++ resources/js/vue/views/Home.vue
@@ -1,6 +1,20 @@
 <template>
-    <div>
-        Hello world
+    <div class="pt-[6.8rem] pb-[4rem]">
+        <div class="container mx-auto">
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-2 px-8 md:px-0">
+                <div class="block flex-1">
+                    <div class="max-w-[480px]">
+                        <h1 class="font-[900px] text-[3rem] leading-[1.5] bold">
+                            Farming Software and Tools for the <span class="text-[#39b54a]">Modern Farmer</span>
+                        </h1>
+                        <p class="font-[500] text-gray-600">Synnefa offers smart and comprehensive farming tools with a variety of products to help the modern farmer take the guesswork out of farming.</p>
+                    </div>
+                </div>
+                <div class="flex items-center justify-center">
+                    <img src="/imgs/Farming-software.png" alt="Farming Software" class="inline-block max-w-full">
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
