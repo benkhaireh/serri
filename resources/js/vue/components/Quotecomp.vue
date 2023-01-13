d2e5e3b384f79b7ab67829db6fb8131516a8a06e
ben Khaireh <62518373+benkhaireh@users.noreply.github.com>
2023-01-13 14:57:35 +0300

    Create Quotecomp.vue

---
+++ resources/js/vue/components/Quotecomp.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Multiselect mode="tags" :close-on-select="false" v-model="value" :options="options" />
+    </div>
+</template>
+<script>
+import Multiselect from "@vueform/multiselect";
+export default {
+    name: "quotecomp",
+    components: { Multiselect },
+    data() {
+        return {
+            value: null,
+            options: ["Batman", "Robin", "Joker"],
+        };
+    },
+};
+</script>
+<style src="@vueform/multiselect/themes/default.css"></style>
